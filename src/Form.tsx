03d97c9073c5e68a7091eb3fa1b97adc0d8126f1
Amirhossein <amirhossein.br.sir@gmail.com>
2023-09-09 20:57:35 +0330

    add select state to handling user element click

--- src/Form.tsx
+++ src/Form.tsx
@@ -2,30 +2,15 @@ import { FormProvider, useForm } from "react-hook-form";
 import { DynamicFieldData } from "./DynamicControlTypes";
 import { DynamicControl } from "./DynamicControl";
 import { ErrorMessage } from "@hookform/error-message";
-import { fields } from "./Data";
-
-const renderFields = (fields, errors) => {
-  return fields.map((field, index) => (
-    <div key={index}>
-      <label htmlFor={field.fieldName}>{field.label}</label>
-      <DynamicControl {...field} />
-      <ErrorMessage errors={errors} name={field.fieldName} />
-
-      {/* Check if the field has children */}
-      {field.children && field.children.length > 0 && (
-        <div>
-          {renderFields(field.children, errors)} {/* Recursive call */}
-        </div>
-      )}
-    </div>
-  ));
-};
+import { useState } from "react";
 
 interface FormProps {
   fields: DynamicFieldData[];
 }
 
 export const Form = ({ fields }: FormProps) => {
+  const [selectedField, setSelectedField] = useState(null);
+
   const formMethods = useForm();
 
   const {
@@ -37,6 +22,30 @@ export const Form = ({ fields }: FormProps) => {
     // your logic on what to do with data
     console.log(data);
   }
+
+  const renderFields = (fields, errors) => {
+    return fields.map((field, index) => (
+      <div
+        key={index}
+        onClick={() => setSelectedField(field.fieldName)} // Set selectedField when clicked
+        style={{
+          cursor: "pointer",
+          border: field.fieldName === selectedField ? "2px solid blue" : "none", // Add styling for selected field
+        }}
+      >
+        <label htmlFor={field.fieldName}>{field.label}</label>
+        <DynamicControl {...field} />
+        <ErrorMessage errors={errors} name={field.fieldName} />
+
+        {/* Check if the field has children */}
+        {field.children && field.children.length > 0 && (
+          <div>
+            {renderFields(field.children, errors)} {/* Recursive call */}
+          </div>
+        )}
+      </div>
+    ));
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
